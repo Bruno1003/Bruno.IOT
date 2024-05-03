6cb189769b780365ff98e624864fcb055b0a51eb
Bruno1003 <168582613+Bruno1003@users.noreply.github.com>
2024-05-03 01:30:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pandas as pd
-#from flask import Flask
+from flask import Flask
 
-#app=Flask(_name_)
+app=Flask(_name_)
 
 base=pd.read_excel("Clasificacion de botes.xlsx")
 
-#@app.route("/")
+@app.route("/")
 def Principal():
     return "Esta es una Api que te muestra los botes de la facultad"
 
-#@app.route("/Por_Tipo/<Tipo>")
+@app.route("/Por_Tipo/<Tipo>")
 def PorTipo(Tipo):
   fila=base[base["Tipo"]==Tipo]
   respuesta=f"El tipo de bote es {fila.loc[:,'Tipo']}"
@@ -17,7 +17,7 @@ def PorTipo(Tipo):
 
 print (PorTipo('Botellas de PET'))
 
-#@app.route("/Por_Numero/<Numero>")
+@app.route("/Por_Numero/<Numero>")
 def PorNumero(Numero):
   Ubicacion=int(Numero)
   resultados=base[base["Longitud"]==Numero]
@@ -26,7 +26,7 @@ def PorNumero(Numero):
 
 print (PorNumero(7))
 
-#@app.route("/Por_Tamaño/<Tamaño>")
+@app.route("/Por_Tamaño/<Tamaño>")
 def PorTamaño(Tamaño):
   resultados=base[base["Tamaño"]==Tamaño]
   resultados=str(resultados)
