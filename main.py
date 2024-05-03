a8609afb3297a38bfc68abe62bfff6e29e2f53c4
Bruno1003 <168582613+Bruno1003@users.noreply.github.com>
2024-05-03 01:29:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
+import pandas as pd
+#from flask import Flask
 
-app=Flask(__name__)
+#app=Flask(_name_)
 
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+base=pd.read_excel("Clasificacion de botes.xlsx")
 
-if __name__=="__main__":
-  app.run()
+#@app.route("/")
+def Principal():
+    return "Esta es una Api que te muestra los botes de la facultad"
+
+#@app.route("/Por_Tipo/<Tipo>")
+def PorTipo(Tipo):
+  fila=base[base["Tipo"]==Tipo]
+  respuesta=f"El tipo de bote es {fila.loc[:,'Tipo']}"
+  return respuesta
+
+print (PorTipo('Botellas de PET'))
+
+#@app.route("/Por_Numero/<Numero>")
+def PorNumero(Numero):
+  Ubicacion=int(Numero)
+  resultados=base[base["Longitud"]==Numero]
+  resultados=str(resultados)
+  return resultados
+
+print (PorNumero(7))
+
+#@app.route("/Por_Tamaño/<Tamaño>")
+def PorTamaño(Tamaño):
+  resultados=base[base["Tamaño"]==Tamaño]
+  resultados=str(resultados)
+  return resultados
+
+print (PorTamaño('Grande'))
